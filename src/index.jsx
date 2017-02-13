264373283f0fb2220ab05b633860d28101075a13
luojionghuang <luojionghuang@gmail.com>
2017-02-13 22:32:24 +0800

    route update, but failed

--- src/index.jsx
+++ src/index.jsx
@@ -1,7 +1,7 @@
 import React from 'react';  // 引入React
 import ReactDOM from 'react-dom'; // 引入render方法
 import { Provider } from 'react-redux'; // 利用Provider可以使我们的 store 能为下面的组件所用
-import { Router, browserHistory } from 'react-router'; // Browser history 是由 React Router 创建浏览器应用推荐的 history
+import { Router, browserHistory, hashHistory } from 'react-router'; // Browser history 是由 React Router 创建浏览器应用推荐的 history
 import { syncHistoryWithStore } from 'react-router-redux'; // 利用react-router-redux提供的syncHistoryWithStore我们可以结合store同步导航事件
 
 import finalCreateStore from './store';  //引入store配置
@@ -12,13 +12,11 @@ import routes from './routes';   // 引入路由配置
 const store = finalCreateStore(reducer);
 
 // 创建一个增强版的history来结合store同步导航事件
-const history = syncHistoryWithStore(browserHistory, store);
+const history = syncHistoryWithStore(hashHistory, store);
 
 ReactDOM.render(
 	<Provider store={store}>
-		<div>
-			<Router history={history} routes={routes} />
-		</div>
+		<Router history={history} routes={routes} />
 	</Provider>,
 	document.getElementById('root')
 );
--- src/routes.jsx
+++ src/routes.jsx
@@ -2,8 +2,25 @@ import React from 'react'; // 引入react
 import { Route, IndexRoute } from 'react-router'; // 引入react路由
 import { Equipment } from './containers'; // 引入各容器组件
 
+const wrapper = (
+	<div>{this.props.children}</div>
+);
+
+const Wrapper = React.createClass({
+	render() {
+		return (
+			<div>
+				{this.props.children}
+			</div>
+		)
+	}
+});
+
 const routes = (
-	<Route path="/" component={Equipment} />
+	<Route path="/" component={Wrapper}>
+		<IndexRoute component={Equipment} />
+		<Route path="/equipment" component={Equipment} />
+	</Route>
 );
 
 module.exports = routes;
